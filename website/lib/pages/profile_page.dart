2729707f29cff1c09486f6b4b0cb777c21daf68c
Levondos <levgrank@gmail.com>
2024-12-06 13:52:44 +0300

    страница профиля без редактирования

--- website/lib/pages/profile_page.dart
+++ website/lib/pages/profile_page.dart
@@ -1,6 +1,8 @@
 import 'package:website/pages/template_page.dart';
 import '../services/api_manager.dart';
 import '../widgets/nav_button.dart';
+import 'package:flutter/material.dart';
+
 
 class ProfilePage extends TemplatePage {
   final String title = 'Profile Page';
@@ -8,4 +10,47 @@ class ProfilePage extends TemplatePage {
 
   const ProfilePage(ApiManager apiManager, {super.key})
       : super(apiManager: apiManager);
+
+  @override
+  Widget getMainArea() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                  'https://via.placeholder.com/150', // Ссылка на аватарку
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Никнейм: FlutterDev',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Описание: Программирую на Flutter, люблю создавать приложения!',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Цель/мотивация: Улучшить привычки и достичь успеха!',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Telegram: @flutterdev',
+              style: const TextStyle(fontSize: 16, color: Colors.blue),
+            ),
+            const SizedBox(height: 100),
+          ],
+        ),
+      ),
+    );
+  }
 }
